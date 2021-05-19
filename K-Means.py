856d35158689c368bfc698d57869d7dffb1d5792
Sebastian Oliver <sebtheoliver@gmail.com>
2021-05-19 17:52:02 +0200

    Added random initialization to centroids.

--- K-Means.py
+++ K-Means.py
@@ -1,8 +1,19 @@
 # Implementation of the K-Means algorithm.
 # Sebastian Oliver
 
+import random
+
 def k_means(dataset):
-    print("in k means")
+
+    clusters = 3
+
+    # Initializing data into random clusters.
+    for i in range(len(dataset)):
+        dataset[i].append(random.randint(1, clusters))
+        print(dataset[i])
+
+    centroids = []
+
 
 def main():
 
@@ -16,7 +27,10 @@ def main():
     dataset = []
     for i in range(size):
         coords = input()
-        coords.split(" ")
+        coords = coords.split()
+        coords[0] = int(coords[0])
+        coords[1] = int(coords[1])
+        
         dataset.append(coords)
 
     k_means(dataset)
