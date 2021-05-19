ecdf276f97d29a3f61f051e6be88d4ce97cefa25
Sebastian Oliver <sebtheoliver@gmail.com>
2021-05-19 17:09:50 +0200

    Created the source file and implemented user input for the dataset.

---
+++ K-Means.py
@@ -0,0 +1,13 @@
+# Implementation of the K-Means algorithm.
+# Sebastian Oliver
+
+print("Hello")
+print("----------------------------")
+
+size = input("Enter the size of the dataset:\n")
+print("----------------------------")
+
+print("Enter the coordinates of the data below in the form <x> <y>:\n")
+dataset[size]
+for i in range(size):
+    dataset.append(input())
