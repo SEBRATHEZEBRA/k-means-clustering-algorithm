038c2e94aa6ad8984475935cde5cf34beddcf67b
Sebastian Oliver <sebtheoliver@gmail.com>
2021-05-20 14:43:49 +0200

    Added a euclidean distance function and a getClosestCentroid function.

--- K-Means.py
+++ K-Means.py
@@ -2,8 +2,31 @@
 # Sebastian Oliver
 
 import random
+import math
 
-def k_means(dataset):
+dataset = []
+centroids = []
+
+def distance(x1, y1, x2, y2):
+
+    dist = math.sqrt((x1 -x2) ^ 2 + (y1 - y2) ^ 2)
+    return dist
+
+# Returns the index of the closest centroid to a point(x, y)
+def getClosestCentroid(x, y):
+
+    index = centroids[0]
+    closest = distance(x, y, dataset[centroid[0]][0], dataset[centroid[0]][1])
+
+    for i in range(1, len(centroids)):
+        dist = distance(x, y, dataset[centroid[i]][0], dataset[centroid[i]][1])
+        if dist < closest:
+            closest = dist
+            index = centroids[i]
+
+    return index
+
+def k_means():
 
     clusters = 3
 
@@ -12,7 +35,18 @@ def k_means(dataset):
         dataset[i].append(random.randint(1, clusters))
         print(dataset[i])
 
-    centroids = []
+    centroids.append(0)
+    centroids.append(3)
+    centroids.append(6)
+
+
+    for i in range(clusters):
+        dataset[centroids[i]][2] = i + 1
+
+    closest = -1
+    for i in range(len(dataset)):
+        closest = getClosestCentroid(dataset[i][0], dataset[i][1])
+        dataset[i][2] = closest
 
 
 def main():
@@ -24,16 +58,18 @@ def main():
     print("----------------------------------------------------------------")
 
     print("Enter the coordinates of the data below in the form <x> <y>:")
-    dataset = []
     for i in range(size):
         coords = input()
         coords = coords.split()
         coords[0] = int(coords[0])
         coords[1] = int(coords[1])
-        
+
         dataset.append(coords)
 
-    k_means(dataset)
+    #k_means()
+
+    #print(distance(1, 4, 3, 7))
+
 
 if __name__ == "__main__":
     main()
